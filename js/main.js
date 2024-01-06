ac34e07bf8164233a9ae441a002d06bc29dd18c6
SaifReact <saifur1985bd@gmail.com>
2024-01-06 21:07:31 +0600

    Update Code

---
+++ js/main.js
@@ -0,0 +1,53 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+	const toggle = document.getElementById(toggleId),
+		nav = document.getElementById(navId)
+
+	if(toggle && nav)
+	{
+		toggle.addEventListener('click', ()=>{
+			nav.classList.toggle('show')
+		})
+	}
+}
+
+showMenu('nav-toggle','nav-menu')
+
+
+/*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction()
+{
+	// Active link
+	navLink.forEach(n => n.classList.remove('active'))
+	this.classList.add('active')
+
+	// Remove menu mobile
+	const navMenu = document.getElementById('nav-menu')
+	navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== AGE CALCULATOR =====*/
+
+function getDOB() { 
+
+    let data = document.getElementById("inputDob").value; 
+
+	let dob = new Date(data); 
+
+	let difference = Date.now() - dob.getTime(); 
+
+	let ageDate = new Date(difference);
+
+	let calculatedAge =   Math.abs(ageDate.getUTCFullYear() - 1970);
+
+	if (calculatedAge >= 18){
+		document.getElementById("result").innerHTML = "Age is:  "  + calculatedAge +"  Years, You are an <b>Adult</b>";
+	} else {
+		document.getElementById("result").innerHTML = "Age is:  "  + calculatedAge +"  Years, You are a <b>Child</b>";
+	}
+
+}
